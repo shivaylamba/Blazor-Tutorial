b59bec2f47c3e36105b7cb01f625fcb849810aab
Shivay Lamba <shivaylamba@gmail.com>
2019-09-28 11:26:29 +0530

    first commit

---
+++ BlazorApp1/Pages/demo.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorApp1.Pages
+{
+    public class demoModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
